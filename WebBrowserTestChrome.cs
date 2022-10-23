669eae4bc33fdcefa5e372df66714f65319e6db1
Vaidiis17 <vaidiis17@gmail.com>
2022-10-23 13:18:50 +0300

    commit

---
+++ WebBrowserTestChrome.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+
+namespace ClassLibrary1
+{
+    class WebBrowserTestChrome
+    {
+        private static IWebDriver chromeDriver;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetup()
+        {
+            chromeDriver = new ChromeDriver();
+            chromeDriver.Url = "https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent";
+            chromeDriver.Manage().Window.Maximize();
+            chromeDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            chromeDriver.Quit();
+        }
+
+        [Test]
+        public static void WebBrowserTestBox()
+        {
+            IWebElement browserVersionText = chromeDriver.FindElement(By.CssSelector(".simple-major"));
+            Assert.IsTrue("Chrome 106 on Windows 10".Equals(browserVersionText.Text),
+                "Button value is not correct");
+        }
+    }
+}
---
+++ WebBrowserTestFirefox.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using System;
+
+namespace ClassLibrary1
+{
+    class WebBrowserTestFirefox
+    {
+        private static IWebDriver firefoxDriver;
+
+        [OneTimeSetUp]
+        public static void OneTimeSetup()
+        {
+            firefoxDriver = new FirefoxDriver();
+            firefoxDriver.Url = "https://developers.whatismybrowser.com/useragents/parse/?analyse-my-user-agent=yes#parse-useragent";
+            firefoxDriver.Manage().Window.Maximize();
+            firefoxDriver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+
+        }
+
+        [OneTimeTearDown]
+        public static void OneTimeTearDown()
+        {
+            firefoxDriver.Quit();
+        }
+
+        [Test]
+        public static void WebBrowserTestBox()
+        {
+            IWebElement browserVersionText = firefoxDriver.FindElement(By.CssSelector(".simple-major"));
+            Assert.IsTrue("Firefox 106 on Windows 10".Equals(browserVersionText.Text),
+                "Button value is not correct");
+        }
+    }
+}
